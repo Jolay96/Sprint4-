bf92a2876ce78cd2b17cf5f46ace72edbfc8ecd7
Jolay96 <Joshua.Olayemi40@gmail.com>
2025-03-04 17:27:55 -0500

    Mybad

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import plotly.express as px
 
 
 
+
 st.header("Market of Vehicles in the US")
 st.write("FIlter the data below to see at what price are cars being sold")
 
 df = pd.read_csv("vehicles_us.csv")
-st.write(df.isnull().sum())
+df.isnull().sum()
 df["model_year"].fillna(df["model_year"].median(), inplace=True)
 df["odometer"].fillna(df["odometer"].median(), inplace=True)
 df["is_4wd"]=df['is_4wd'].fillna(0)
 df["paint_color"].fillna(df["paint_color"].mode()[0], inplace=True)
 df["transmission"].fillna(df["transmission"].mode()[0], inplace=True)
-st.write(df.head())
 
 st.header('Price analysis')
 st.write("#####" " Lets analyze what influences price the most. We will check how distibution of price varies depending on  transmission, engine or body type and state")
